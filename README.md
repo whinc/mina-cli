db84613d195527049e0b3275a593a3e67da9e614
whincwu <whincwu@163.com>
2019-03-10 14:37:34 +0800

    feat: 增加命令行接口

--- README.md
+++ README.md
@@ -43,12 +43,50 @@ mina-cli 是小程序原生开发脚手架。
 |`<style lang="less">`|`less`|
 |`<script>`|`js`|
 
+## 使用
+
+安装 mina
+```
+npm install -g mina
+```
+
+命令行参数配置
+```bash
+mina build --src <path_to_source> --dest <path_to_output>
+```
+
+或者通过`--config`指定配置文件
+```
+// mina.config.json
+{
+  "src": "example/src",
+  "dest": "example/dist"
+}
+
+// 执行指令
+mina --config mina.config.json
+```
+
+或者在`package.json`中通过`mina`字段指定配置
+```
+// package.json
+{
+  "mina": {
+    "src": "example/src",
+    "dest": "example/dist"
+  }
+}
+
+// 执行指令
+mina
+```
+
 ## 配置语法高亮
 
 **Virtual Studio Code**
 
 1. 安装 Vetur 插件
-2. 运行命令 "Preferences: Open Settings (JSON)"
+2. 运行命令 "Preferences: Open Settings (JSON)"，添加下面配置
 ```json
   "files.associations": {
     "*.mina": "vue",
@@ -60,9 +98,11 @@ mina-cli 是小程序原生开发脚手架。
     "*.acss": "css",
     "*.axml": "html",
   },
+```
+3. 运行命令 "Vetur: Generate grammar from `vetur.grammar.customBlocks`"，添加下面配置
+```json
   "vetur.grammar.customBlocks": {
     "config": "json"
   }
 ```
-3. 运行命令 "Vetur: Generate grammar from `vetur.grammar.customBlocks`"
 4. 运行命令 "Reload Window"
--- bin/mina.js
+++ bin/mina.js
@@ -1,5 +1,87 @@
 #!/usr/bin/env node
 
 'use strict';
+const yargs = require('yargs')
+const gulp = require('gulp')
+const path = require('path')
+const {updateConfig, clean, compileAll, watchAll, test} = require('..')
 
-require('../')();
+const argv = yargs
+  .usage('Usage: $0 <command> [options...]')
+  .config()
+  .pkgConf('mina')
+  .command('clean', 'clean output directory',
+    yargs => yargs.options({
+      'dest': {
+        demandOption: true,
+        describe: 'The directory will be clean'
+      }
+    }).version(false).help(false),
+    argv => {
+      gulp.series(updateConfig({
+        dstDir: argv.dest
+      }), clean)()
+    })
+  .command(['build', '$0'], 'compile source directory and output',
+    yargs => yargs.options({
+      'src': {
+        type: 'string',
+        demandOption: true,
+        describe: 'source directory'
+      },
+      'dest': {
+        type: 'string',
+        demandOption: true,
+        describe: 'output directory'
+      },
+      'include': {
+        type: 'array',
+        default: ['**/*'],
+        describe: 'these files should be included (glob pattern)'
+      },
+      'exclude': {
+        type: 'array',
+        default: [],
+        describe: 'these files should be excluded (glob pattern)'
+      },
+      'target': {
+        type: 'string',
+        choices: ['wx', 'my'],
+        describe: 'target platform, support: "wx"(weixin), "my"(alipay)',
+        default: 'wx'
+      },
+      'watch': {
+        alias: 'w',
+        type: 'boolean',
+        describe: 're-compile when any file in the src directory changed'
+      }
+    }).version(false).help(false),
+    argv => {
+      const config = {
+        srcDir: path.resolve(process.cwd(), argv.src),
+        dstDir: path.resolve(process.cwd(), argv.dest),
+        include: argv.include,
+        exclude: argv.exclude,
+        target: argv.target
+      }
+
+      // mina --src ./example/src --dest ./example/dist --include '**/*' '*.a' --exclude '**/*.md' --target my
+      console.log('config:', config)
+      const tasks = [updateConfig(config), clean, compileAll]
+      if (argv.watch) {
+        tasks.push(watchAll)
+      }
+      gulp.series(tasks)()
+    })
+  .demandCommand()
+  .version()
+  .alias('v', 'version')
+  .help()
+  .alias('h', 'help')
+  .showHelpOnFail(true)
+  .argv
+
+
+gulp.on('error', err => {
+  console.error(err)
+})
